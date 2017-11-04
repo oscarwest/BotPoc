3094d5089cea87730f6cb4add82a93ecb2c7c279
Oscar West <oscar.west@collectorbank.se>
2017-11-04 16:28:45 +0100

    Add nested dialogs

--- BotPoc/Dialogs/Payments/PaymentsDialog.cs
+++ BotPoc/Dialogs/Payments/PaymentsDialog.cs
@@ -13,20 +13,14 @@ namespace BotPoc.Dialogs.Payments
     {
         public async Task StartAsync(IDialogContext context)
         {
-            var msg = context.MakeMessage();
-            msg.Type = ActivityTypes.Message;
-            msg.TextFormat = TextFormatTypes.Plain;
-            msg.SuggestedActions = new SuggestedActions()
+            var msg = await this.CreateCardActionMessage(context, new List<CardAction>()
             {
-                Actions = new List<CardAction>()
-                {
-                     new CardAction() { Title = "Mina fakturor", Type = ActionTypes.ImBack, Value = "someValue" },
-                     new CardAction() { Title = "Ränta och avgifter", Type = ActionTypes.ImBack, Value = "someValue" },
-                     new CardAction() { Title = "Leverans och retur", Type = ActionTypes.ImBack, Value = "someValue" },
-                     new CardAction() { Title = "Kreditupplysningar", Type = ActionTypes.ImBack, Value = "someValue" },
-                     new CardAction() { Title = "Bedrägeri", Type = ActionTypes.ImBack, Value = "someValue" }
-                }
-            };
+                new CardAction() { Title = "Mina fakturor", Type = ActionTypes.PostBack, Value = "Mina fakturor" },
+                new CardAction() { Title = "Ränta och avgifter", Type = ActionTypes.ImBack, Value = "Ränta och avgifter" },
+                new CardAction() { Title = "Leverans och retur", Type = ActionTypes.ImBack, Value = "Leverans och retur" },
+                new CardAction() { Title = "Kreditupplysningar", Type = ActionTypes.ImBack, Value = "Kreditupplysningar" },
+                new CardAction() { Title = "Bedrägeri", Type = ActionTypes.ImBack, Value = "Bedrägeri" }
+            });
 
             await context.PostAsync(msg);
 
@@ -36,10 +30,50 @@ namespace BotPoc.Dialogs.Payments
         public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
             var message = await result;
+            var messageText = message.Text;
+            IMessageActivity msg = null;
 
-            await context.PostAsync("paymentDialog received");
+            switch (messageText)
+            {
+                case "Mina fakturor":
+                    msg = await this.CreateCardActionMessage(context, new List<CardAction>()
+                    {
+                        new CardAction() { Title = "Fakturaleverans", Type = ActionTypes.PostBack, Value = "Fakturaleverans", DisplayText = "Fakturaleverans info displaytext", Text = "Fakturaleverans info text" }
+                    });
+                    await context.PostAsync(msg);
+                    break;
+                case "Fakturaleverans":
+                    msg = await this.CreateCardActionMessage(context, new List<CardAction>()
+                    {
+                        new CardAction() { Title = "Hur skickas min faktura?", Type = ActionTypes.PostBack, Value = "Hur skickas min faktura?" },
+                        new CardAction() { Title = "Varför ser jag inte min faktura?", Type = ActionTypes.PostBack, Value = "Varför ser jag inte min faktura?" },
+                        new CardAction() { Title = "Samma faktura på flera sätt", Type = ActionTypes.PostBack, Value = "Samma faktura på flera sätt" },
+                        new CardAction() { Title = "Varför har jag fått en faktura från Collector?", Type = ActionTypes.PostBack, Value = "Varför har jag fått en faktura från Collector?" }
+                    });
+                    await context.PostAsync(msg);
+                    break;
 
-            context.Done(this);
+                default:
+                    break;
+            }
+
+
+            //await context.PostAsync("paymentDialog received");
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        private async Task<IMessageActivity> CreateCardActionMessage(IDialogContext context, List<CardAction> cardActions)
+        {
+            var msg = context.MakeMessage();
+            msg.Type = ActivityTypes.Message;
+            msg.TextFormat = TextFormatTypes.Plain;
+            msg.SuggestedActions = new SuggestedActions()
+            {
+                Actions = cardActions
+            };
+
+            return await Task.FromResult(msg);
         }
     }
 }
